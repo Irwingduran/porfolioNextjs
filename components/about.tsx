2135ccc6e7918e030d04c5d656f9e8c0c02ba477
Irwing <86335744+Irwingduran@users.noreply.github.com>
2023-08-12 15:21:12 -0600

    Add files via upload

---
+++ components/about.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I'm about to finish my degree in{" "}
+        <span className="font-medium">Computer Science Engineering</span>, and
+        before entering the university I learned how to program in {" "}
+        <span className="font-medium">Javascript</span>.{" "}I enrolled in a
+        <span className="italic">programming course and learned full web development.</span>
+        I have a flexible schedule that allows me to find <span className="underline">Full Time Work.</span>
+        My favorite part of programming is the feeling of finally finding a solution to a problem.
+        My main stack is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>.
+        I am also familiar with TypeScript. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">Full-Time</span>position as a software
+        developer.
+      </p>
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="font-medium">take pictures</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,102 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Computer Science Engineering",
+    location: "Puebla, Pue",
+    description:
+      "I am not far from finishing college, so I started to show my skills before the degree.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Puebla, Pue",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2021 - 2023",
+  },
+  {
+    title: "Freelance Developer",
+    location: "Mexico City, Mx",
+    description:
+      "I'm now a javascript developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2023 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "3D Website",
+    description:
+      "My personal project was implementing my front-end knowledge and implementing some 3D modes.",
+    tags: ["React", "Next.js", "Vite.js", "Tailwind", "Blender"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "Tailwind", "MongoDB"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Express",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
